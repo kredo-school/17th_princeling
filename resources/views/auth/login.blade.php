bbdf3aad581ad71bbb5c5f0c2daa71f4d4228d23
Chihiro Fujii <fujichippy@gmail.com>
2024-01-23 20:01:11 +0900

    added changes to the login UI.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,9 +3,37 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-7">
+            <img src="{{ asset('img/login.jpeg') }}" class="img-size" alt="Login Image">
+        </div>
+
+        {{-- Create login --}}
+        <div class="col">
+            <h2 class="fw-bold">Log in</h2>
+            <p>Don't have your account? <a href="{{ route('register') }}" class="text-decoration-none text-dark fw-bold">Sign up</a></p>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" id="email"  class="form-control">
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password"  class="form-control">    
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col text-center">
+                    <button type="submit" class="btn button-color1 text-white">Log in</button>
+                </div>
+            </div>
+        </div>
+
+        {{-- <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -67,7 +95,7 @@
                     </form>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,50 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-7">
+            <h2>put image here</h2>
+        </div>
+
+        {{-- Create register --}}
+        <div class="col">
+            <h2 class="fw-bold">Create an Account</h2>
+            <p>Have your account? <a href="{{ route('login') }}" class="text-decoration-none text-dark fw-bold">login</a></p>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <label for="first-name" class="form-label">First Name</label>
+                    <input type="text" name="first-name" id="first-name"  class="form-control">
+                </div>
+                <div class="col">
+                    <label for="last-name" class="form-label">Last Name</label>
+                    <input type="text" name="last-name" id="last-name"  class="form-control">
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" id="email"  class="form-control">
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password"  class="form-control">    
+                </div>
+            </div>
+            <div class="row mt-2">
+                <div class="col">
+                    <label for="confirm-password" class="form-label">Confirm Password</label>
+                    <input type="password" name="confirm-password" id="confirm-password"  class="form-control">    
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col text-center">
+                    <button type="submit" class="btn button-color1 text-white">Sign up</button>
+                </div>
+            </div>
+        </div>
+        {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
@@ -71,7 +114,7 @@
                     </form>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
