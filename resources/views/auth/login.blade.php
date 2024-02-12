79448c49e3aa3b7d72319cebefa65fac5ebed53c
lunarkin <133897017+lunarkin@users.noreply.github.com>
2024-02-12 19:55:45 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="loginpage">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-7">
-                    <img src="{{ asset('img/login.jpeg') }}" class="img-size" alt="Login Image">
-                </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-7">
+            <img src="{{ asset('img/login.jpeg') }}" class="img-size" alt="Login Image">
+        </div>
 
         {{-- Create login --}}
         <div class="col">
@@ -60,30 +59,43 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                {{-- Create login --}}
-                <div class="col">
-                    <h2 class="fw-bold">Log in</h2>
-                    <p>Don't have your account? <a href="{{ route('register') }}" class="text-decoration-none text-dark fw-bold">Sign up</a></p>
-                    <br>
-                    <div class="row">
-                        <div class="col">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="email" name="email" id="email"  class="form-control">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    <div class="row mt-2">
-                        <div class="col">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" id="password"  class="form-control">    
+
+                        <div class="row mb-3">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row mt-4">
-                        <div class="col text-center">
-                            <button type="submit" class="btn button-color1 text-white">Log in</button>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div> --}}
     </div>
+</div>
 @endsection
